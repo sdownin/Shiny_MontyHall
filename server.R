2ef42a1bbc26dba137eef2804e5bca9473ef4d7a
sdownin <sdowning.bm02g@nctu.edu.tw>
2014-08-23 01:38:44 +0800

    ui and server files updated pre first push

--- server.R
+++ server.R
@@ -6,19 +6,90 @@
 #
 
 library(shiny)
-source('MontyHall.R')  #MontyHallStrat() function
+source('MontyHall.R')
 
 shinyServer(function(input, output) {
-  # generate values based on input$____ from ui.R   
-  x <- MontyHallStrat(games = input$games, doors = input$doors, strategy = input$strategy, set.seed = 1)
   
-  output$stratPlot <- renderPlot({ 
+  output$stratPlot <- renderPlot({
     
-    #par(xpd=F) # keep abline inside plot frame
-    #par(mfrow=c(1,1))
-    plot(x,type='l',main=paste("Mean After",games,"Games :",m),ylab="probability",lwd=2,ylim=c(0,1));abline(h= 2/3, col='red')
+    # generate bins based on input$bins from ui.R
+    g <- input$games
+    #     d <- as.numeric(input$doors)
+    d <- 3  # address number or doors in later shiny app
+    x    <- MontyHallStrat(games = g,doors = d,strategy = input$strategy,set.seed = 1) 
     
+    # draw the histogram with the specified number of bins
+    par(mfrow=c(1,2))
+    if (input$strategy=='switch') {
+      
+      plot(x, type='l',main=paste("You Chose Wisely!\nMean After ",g," Games: ",round(mean(x),4)),
+           xlab='Number of Games',
+           ylab = "Probability",
+           lwd=2,ylim=c(0,1), col='dark green');
+      abline(h=2/3,col='red')
+      # draw the histogram with the specified number of bins
+      plot(density(x), main="Density of Cumulative Probability Avg.s",xlim=c(.1,.9)); abline(v=2/3,col='red')
+      
+    } else if (input$strategy=='random') {
+      
+      plot(x, type='l',main=paste("Make a Call...Switch or Stay\nRandom Mean After ",g," Games: ",round(mean(x),2)),
+           xlab='Number of Games',
+           ylab = "Probability",
+           lwd=2,ylim=c(0,1) );
+      abline(h=1/2,col='red')
+      # draw the histogram with the specified number of bins
+      plot(density(x), main="Density of Cumulative Probability Avg.s",xlim=c(.1,.9)); abline(v=1/2,col='red')
+      
+    } else {   #if strategy == 'stay'
+      
+      plot(x, type='l',main=paste("You Chose Poorly :(\nMean After ",g," Games: ",round(mean(x),4)),
+           xlab='Number of Games',
+           ylab = "Probability",
+           lwd=2,ylim=c(0,1), col='dark gray');
+      abline(h=1/3,col='red')
+      # draw the histogram with the specified number of bins
+      plot(density(x), main="Density of Cumulative Probability Avg.s",xlim=c(.1,.9)); abline(v=1/3,col='red')
+      
+    }
     
-  })
+  })  #end renderPlot
+  ###################################################################
   
-})
+  output$doorsPlot <- renderPlot({
+    
+    g <- input$games
+    d <- 3:17
+    
+    swi <- numeric(15)
+    ran <- numeric(15)
+    sta <- numeric(15)
+    for (i in 1:15) {
+      swi[i] <- mean(MontyHallStrat(games = g,doors = 2+i,strategy = "switch",set.seed = 1))
+      ran[i] <- mean(MontyHallStrat(games = g,doors = 2+i,strategy = "random",set.seed = 1))
+      sta[i] <- mean(MontyHallStrat(games = g,doors = 2+i,strategy = "stay",set.seed = 1))
+    }
+    
+    plot(x = 3:17,y=swi, type='o',xlab="Doors",ylab='Probability', 
+         main=paste('Empirical Probabilities of Success by Strategy for ',
+                    g,
+                    ' Games\n(dotted black lines show curves of analytical solutions)'),
+         col='green',pch=20)
+    legend(x = 12,y=.6,legend = c('switch','random','stay'),col = c('green','blue','red'),lty=1)
+    text(x = 3:17,y = swi,labels = round(swi,3),pos = c(4),col='green')
+    lines(x = 3:17,y=ran, type='o',col='blue',pch=20)
+    text(x=3:7,y=ran[1:5],labels=round(ran[1:5],3),pos=c(1),col='blue')
+    lines(x = 3:17,y=sta, type='o',col='red',pch=20)
+    text(x=3:22,y=sta,labels=round(sta,3),pos=c(1),col='red')
+    x <- seq(from = 3,to = 17, length.out = 200)
+    y1 <- (x-1)/(x*(x-2))
+    y2 <- 1/(x-1)
+    y3 <- 1/x
+    lines(x,y1,type='l',col='black',lty=2)
+    lines(x,y2,type='l',col='black',lty=2)
+    lines(x,y3,type='l',col='black',lty=2)
+ 
+    
+    })
+
+  
+})  #end shinyServer function
--- ui.R
+++ ui.R
@@ -6,37 +6,60 @@
 #
 
 library(shiny)
+source('MontyHall.R')
 
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(responsive = T,
   
   # Application title
-  headerPanel("Let's Make a Deal! What Should You Do?"),
+  #   headerPanel("Monty Hall Simulation"),
+  titlePanel("Let's Make a Deal!"),
   
   # Sidebar with a slider input for number of bins
-  sidebarPanel(
-    h1("What's your game strategy?"),
-    selectInput("strategy", 
-                "Choose a strategy x:",
-                choices = c('switch','random','stay'), 
-                selected = 'switch'), 
-    h2("How many games do you get to play?"),
-    sliderInput("games",
-                "Number of games:",
-                min = 1,
-                max = 50000,
-                value = 100,
-                step = 1,
-                animate = T),
-    h2("What happens if Monty adds more doors?"),
-    selectInput("doors",
-                "Number of doors:",
-                choices = c(3,4,5,6,7,8,9,10,11,
-                            12,13,14,15,20,50,100),
-                selected = 3),
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("stratPlot")
-  )
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      p("What would you do if you had a chance to stand next to Monty Hall on Let's Make a Deal! and choose which door has the car, and then--after Monty opens one door revealing a goat (ZONK!)--finally choose to stay with your original choice or switch doors? Which strategy would you choose:",style="color:green"),
+      p(strong("1. Switch doors")),
+      p(strong("2. Randomly choose")),
+      p(strong("3. Stay with 1st Choice")),
+      p("Hint: The correct decision--and there is a right and wrong here--can be empirically supported with simulation showing which strategy has the highest expected probability of success in the long run.", style="color:green"),
+      
+      selectInput("strategy",
+                  em("Choose a strategy:",style="color:red"),
+                  c("switch" = "switch",
+                    "random" = "random",
+                    "stay" = "stay"),
+                  selected="random"),
+      p('Want to understand why? This lecture explains it all:', style="color:gray"),
+      a("Harvard Stat 110: Probability - Lec.6",href="https://www.youtube.com/watch?v=fDcjhAKuhqQ",target="_blank"),
+      
+#       submitButton("Choose a Door!"),
+      
+      h4("How many games do you get to play?"),
+      
+      sliderInput("games",
+                  "Number of games:",
+                  min = 100,
+                  max = 2000,
+                  value = 100,
+                  step = 100)
+      
+    ),  #end sidePanel
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      h3("What are your chances of success?"),
+      h5("(3-Door Game)")
+      ,
+      plotOutput("stratPlot")
+      ,
+      h3("But what if Monty added more doors?")
+      ,
+      #       plotOutput("histPlot"),
+      plotOutput("doorsPlot")
+      
+    ) #end mainPanel
+    
+  )  #end sidebarLayout
 ))
